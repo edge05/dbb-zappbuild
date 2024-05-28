5275c2d8fe5da728eb3212d1c82e06d736d6e6a7
Jerry Edgington <Jerry.Edgington@Mainline.com>
2024-05-28 12:53:35 -0400

    added missing () to CopyToPDS

--- samples/groovy/copyToPDS.groovy
+++ samples/groovy/copyToPDS.groovy
@@ -1,7 +1,8 @@
 import com.ibm.dbb.build.CopyToPDS
 println('Copy USS file to PDS as member')
-def copyFile = new CopyToPDS
-copyFile.setDataset("JEDGING.TEST.PDS")
+PDSname = "JEDGING.TEST.PDS"
+def copyFile = new CopyToPDS()
+copyFile.setDataset(PDSname)
 //
 copyFile.setFile(new File("./SYSIN/file1.txt"))
 copyFile.setMember("FILE1")
