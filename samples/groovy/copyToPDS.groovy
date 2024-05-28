d1282fd3b8c632b8a691f4e0e2e4f5ef08b26aad
Jerry Edgington <Jerry.Edgington@Mainline.com>
2024-05-28 12:56:18 -0400

    updated directory to SYSIN files

--- samples/groovy/copyToPDS.groovy
+++ samples/groovy/copyToPDS.groovy
@@ -4,10 +4,10 @@ PDSname = "JEDGING.TEST.PDS"
 def copyFile = new CopyToPDS()
 copyFile.setDataset(PDSname)
 //
-copyFile.setFile(new File("./SYSIN/file1.txt"))
+copyFile.setFile(new File("./dbb-zappbuild/samples/groovy/SYSIN/file1.txt"))
 copyFile.setMember("FILE1")
 copyFile.execute()
 //
-copyFile.setFile(new File("./SYSIN/file2.txt"))
+copyFile.setFile(new File("./dbb-zappbuild/samples/groovy/SYSIN/file2.txt"))
 copyFile.setMember("FILE2")
 copyFile.execute()
