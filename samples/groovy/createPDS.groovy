84e176e48facfda28f3392b474f3d7279ad47473
Jerry Edgington <Jerry.Edgington@Mainline.com>
2024-05-28 11:11:27 -0400

    added missing ) to setOptions

--- samples/groovy/createPDS.groovy
+++ samples/groovy/createPDS.groovy
@@ -2,7 +2,5 @@ import com.ibm.dbb.build.*
 println("Create new PDS")
 def newPDS = new CreatePDS()
 newPDS.setDataset("JEDGING.TEST.PDS")
-newPDS.setOptions("cyl space(1,1) lrecl(80) dsorg(PO) recfm(F,B) dsntype(library)"
+newPDS.setOptions("cyl space(1,1) lrecl(80) dsorg(PO) recfm(F,B) dsntype(library)")
 newPDS.execute()
-println("--------------------------")
-println("new PDS created")
